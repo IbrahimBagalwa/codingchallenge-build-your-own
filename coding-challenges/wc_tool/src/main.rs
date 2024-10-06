d6f8c29eb2c4693526230f396dcfb9013934b7ed
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2024-10-06 12:26:10 +0200

    feat: step two count lines of the file

--- coding-challenges/wc_tool/src/main.rs
+++ coding-challenges/wc_tool/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, BufReader, Read};
+use std::io::{self, BufRead, BufReader, Read};
 use std::path::Path;
 
 use clap::{Parser, Subcommand};
@@ -15,7 +15,8 @@ enum Commands{
     /// Count the number of bytes in the text
    #[command(about = "Count the number of bytes in a file", short_flag = 'c')] 
     Count,
-
+  #[command(about = "Count the number of lines in a file", short_flag = 'l')]
+    Line
 }
 
 
@@ -30,6 +31,23 @@ where
     Ok(buffer.len())
 }
 
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+async fn count_lines(filename:String)->anyhow::Result<usize>{
+    let mut count_lines= 0;
+    
+    if let Ok(lines) = read_lines(filename) {
+        lines.for_each(|_|{
+                count_lines += 1;
+        })
+    }
+    Ok(count_lines)
+}
+
 #[tokio::main]
 async fn main()-> anyhow::Result<()>{
     let cli = Args::parse();
@@ -39,6 +57,10 @@ async fn main()-> anyhow::Result<()>{
         Some(Commands::Count) => {
             let count=count_bytes_in_file("Test.txt".to_string())?;
             println!("{count}");
+        },
+        Some(Commands::Line)=> {
+            let lines_number = count_lines("Test.txt".to_string()).await?;
+            println!("{lines_number}")
         },
         None =>println!("Run with --help to see all the commands")
     
