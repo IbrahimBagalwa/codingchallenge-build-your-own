4d8e470c7fb7cc84101714f76b4ee972ba277e54
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2024-10-06 11:59:19 +0200

    feat: step one count the bytes of the lines

--- coding-challenges/wc_tool/src/main.rs
+++ coding-challenges/wc_tool/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufReader, Read};
+use std::path::Path;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command()]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>
+}
+#[derive(Subcommand)]
+enum Commands{
+    /// Count the number of bytes in the text
+   #[command(about = "Count the number of bytes in a file", short_flag = 'c')] 
+    Count,
+
+}
+
+
+fn count_bytes_in_file<P>(filename: P) -> io::Result<usize>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let mut reader = BufReader::new(file);
+    let mut buffer = Vec::new();
+    reader.read_to_end(&mut buffer)?;
+    Ok(buffer.len())
+}
+
+#[tokio::main]
+async fn main()-> anyhow::Result<()>{
+    let cli = Args::parse();
+    
+    
+    match cli.command {
+        Some(Commands::Count) => {
+            let count=count_bytes_in_file("Test.txt".to_string())?;
+            println!("{count}");
+        },
+        None =>println!("Run with --help to see all the commands")
+    
+    };
+    Ok(())
 }
